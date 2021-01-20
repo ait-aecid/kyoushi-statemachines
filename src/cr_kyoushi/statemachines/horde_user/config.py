3aa47a64d6711052d551564bc1fd6b285c797805
Maximilian Frank <maximilian.frank@ait.ac.at>
2021-01-20 13:37:20 +0100

    Add horde user sm base layout

---
+++ src/cr_kyoushi/statemachines/horde_user/config.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+from pydantic import Field
+from selenium import webdriver
+
+from cr_kyoushi.simulation.model import WorkSchedule
+
+from ..core.selenium import SeleniumConfig
+
+
+__all__ = [
+    "StatemachineConfig",
+    "Context",
+]
+
+
+class StatemachineConfig(BaseModel):
+    """Web browser state machine configuration model
+
+    Example:
+        ```yaml
+        max_error: 0
+        start_time: 2021-01-23T9:00
+        end_time: 2021-01-29T00:01
+        schedule:
+        work_days:
+            monday:
+                start_time: 09:00
+                end_time: 17:30
+            friday:
+                start_time: 11:21
+                end_time: 19:43
+        selenium:
+            driver_manager:
+                cache_valid_range: 5 # days
+            type: firefox
+            window_width: 800
+            window_height: 600
+            accept_insecure_ssl: yes
+        ```
+    """
+
+    max_errors: int = Field(
+        0,
+        description="The maximum amount of times to try to recover from an error",
+    )
+
+    start_time: Optional[datetime] = Field(
+        None,
+        description="The state machines start time",
+    )
+
+    end_time: Optional[datetime] = Field(
+        None,
+        description="The state machines end time",
+    )
+
+    schedule: Optional[WorkSchedule] = Field(
+        None,
+        description="The work schedule for the web browser user",
+    )
+
+    selenium: SeleniumConfig = Field(
+        SeleniumConfig(),
+        description="Selenium configuration for the web browser user",
+    )
+
+
+class Context(BaseModel):
+    driver: webdriver.Remote
+    """The selenium web driver"""
+
+    class Config:
+        arbitrary_types_allowed = True
---
+++ src/cr_kyoushi/statemachines/horde_user/sm.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+from typing import List
+from typing import Optional
+
+from cr_kyoushi.simulation import sm
+from cr_kyoushi.simulation import states
+from cr_kyoushi.simulation.model import WorkSchedule
+
+from ..core.selenium import SeleniumConfig
+from ..core.selenium import get_webdriver
+from ..core.selenium import install_webdriver
+from .config import Context
+from .config import StatemachineConfig
+
+
+__all__ = ["Statemachine", "StatemachineFactory"]
+
+
+class Statemachine(sm.WorkHoursStatemachine):
+    _selenium_config: SeleniumConfig
+    _webdriver_path: Optional[str]
+
+    def __init__(
+        self,
+        initial_state: str,
+        states: List[states.State],
+        selenium_config: SeleniumConfig,
+        start_time: Optional[datetime] = None,
+        end_time: Optional[datetime] = None,
+        work_schedule: Optional[WorkSchedule] = None,
+        max_errors: int = 0,
+    ):
+        super().__init__(
+            initial_state,
+            states,
+            start_time=start_time,
+            end_time=end_time,
+            work_schedule=work_schedule,
+            max_errors=max_errors,
+        )
+        self._selenium_config = selenium_config
+        self._webdriver_path = None
+        self.context: Optional[Context] = None
+
+    def setup_context(self):
+        # we assume we only install once at the start of the sm
+        if self._webdriver_path is None:
+            self._webdriver_path = install_webdriver(self._selenium_config)
+
+        self.context = Context(
+            driver=get_webdriver(
+                self._selenium_config,
+                self._webdriver_path,
+            )
+        )
+
+    def destroy_context(self):
+        if self.context is not None:
+            self.context.driver.quit()
+
+    def _resume_work(self):
+        self.current_state = self.initial_state
+        # reset context
+        self.destroy_context()
+        self.setup_context()
+
+
+class StatemachineFactory(sm.StatemachineFactory):
+    @property
+    def name(self) -> str:
+        return "HordeUserStatemachineFactory"
+
+    @property
+    def config_class(self):
+        return StatemachineConfig
+
+    def build(self, config: StatemachineConfig):
+        # setup transitions
+
+        # idle_transition = IdleTransition(
+        #     idle_amount=0.0,
+        #     end_time=config.end_time,
+        #     name="idle",
+        #     target="selecting_activity",
+        # )
+
+        initial = states.FinalState("initial")
+
+        return Statemachine(
+            initial_state="initial",
+            states=[initial],
+            selenium_config=config.selenium,
+            start_time=config.start_time,
+            end_time=config.end_time,
+            work_schedule=config.schedule,
+            max_errors=config.max_errors,
+        )
